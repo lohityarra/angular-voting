facd6d9699c403c2fb499d5d145ccf7b7c6dd98b
Lohit Yarra <43040093+lohityarra@users.noreply.github.com>
2022-03-08 12:56:33 +0530

    first commit voting system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'voting';
+  selected:number = -1;
+
+  onVote(index:number){
+    if(this.isSelected(index)){
+      this.selected= -1;
+    }
+    else{
+      this.selected=index;
+    }
+
+  }
+  isSelected(index:number){
+    return this.selected === index;
+  }
+  anySelected(){
+    return this.selected !== -1;
+  }
+  getButtonText(index:number){
+    if(this.isSelected(index)){
+      return "Unvote";
+    }else{
+      return "Vote";
+    }
+  }
 }
